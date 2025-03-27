891b26979500b0286e5db3095cbaec6c48b25d60
museekee <103117239+museekee@users.noreply.github.com>
2025-03-27 20:27:24 +0900

    sexyenv 추가가

--- src/routes/api/user.ts
+++ src/routes/api/user.ts
@@ -9,15 +9,28 @@ router.post('/updateme', async (req, res) => {
   if (!uid)
     res.status(401).json({ message: 'Not authenticated' })
   else {
+    // 이름: 15글자 이하
+    // 학년: 1~3
+    // 반: 1~6
+    let isValid = true
+    if (req.query.name && req.query.name.toString().length > 15)
+      isValid = false
+    if (req.query.grade && (isNaN(parseInt(req.query.grade.toString())) || parseInt(req.query.grade.toString()) > 3 || parseInt(req.query.grade.toString()) < 1))
+      isValid = false
+    if (req.query.class && (isNaN(parseInt(req.query.class.toString())) || parseInt(req.query.class.toString()) > 6 || parseInt(req.query.class.toString()) < 1))
+      isValid = false
+    if (!isValid)
+      res.status(400).json({ message: 'Bad request' })
+  else {
     const name = !req.query.name ? undefined : req.query.name.toString()
-    const grade = !req.query.grade || isNaN(parseInt(req.query.grade.toString())) ? undefined : parseInt(req.query.grade.toString())
-    const class_ = !req.query.class || isNaN(parseInt(req.query.class.toString())) ? undefined : parseInt(req.query.class.toString())
+    const grade = !req.query.grade ? undefined : parseInt(req.query.grade.toString())
+    const class_ = !req.query.class ? undefined : parseInt(req.query.class.toString())
     const email = !req.query.email ? undefined : req.query.email.toString()
     const profile_image = !req.query.profile_image ? undefined : req.query.profile_image.toString()
     const user: Partial<User> = {
       name: name,
-      grade: grade && grade <= 3 && grade >= 1 ? grade : undefined,
-      class: class_ && class_ <= 6 && class_ >= 1 ? class_ : undefined,
+      grade: grade,
+      class: class_,
       email: email,
       profile_image: profile_image
     }
@@ -27,7 +40,8 @@ router.post('/updateme', async (req, res) => {
     else {
       await updateUser(uid, user)
       res.status(200).json({ message: 'Updated' })
-    }
+      }
+  }
   }
 })
 router.get('/registered', async (req, res) => {
