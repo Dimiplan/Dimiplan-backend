6f8d0d0d580ee631635b00e71e69a1059c1f54f9
Kor-XCoder <mcoder1117@gmail.com>
2025-03-31 17:13:31 +0900

    플래너 기능 추가 및 버그 픽스

--- src/routes/api/plan.js
+++ src/routes/api/plan.js
@@ -1,11 +1,11 @@
 const { getUser, isRegistered, updateUser } = require('../../models/userModel');
 const express = require('express');
-const knex = require('knex')
-
+const db = require('../../config/db');
 const router = express.Router();
 
 router.post('/addTask', async (req, res, next) => {
     const { content, date, priority } = req.body;
+    console.log(content, date, priority);
     const uid =
         req.session &&
         req.session.passport &&
@@ -16,24 +16,40 @@ router.post('/addTask', async (req, res, next) => {
         return;
     }
     const formattedDate = new Date(date).toISOString().slice(0, 10); // "YYYY-MM-DD"
-    // 'tasks' 테이블에서 현재 요청과 owner와 date가 같은 행의 개수를 조회
-    const count = await knex('tasks')
+    let seq;
+    // 'userSeq' 테이블에서 현재 요청과 owner와 date가 같은 행이 있는지 확인, 만약 있으면 seq 값을 가져오고, 없으면 owner, date, seq=0로 된 행을 insert
+    const count = await db('userSeq')
         .where({ owner: uid, date: formattedDate })
-        .count('* as cnt')
-        .then(rows => rows[0].cnt);
-    const seq = count + 1; 
+        .count('seq as count')
+        .first();
+    if (count.count > 0) {
+        const seqResult = await db('userSeq')
+            .where({ owner: uid, date: formattedDate })
+            .select('seq')
+            .first();
+        seq = seqResult.seq;
+    } else {
+        const seqResult = await db('userSeq')
+            .insert({ owner: uid, date: formattedDate, seq: 0 })
+        seq = 0
+    }
+    seq++;
+    // 'userSeq' 테이블에서 seq 값을 1 추가해 업데이트
+    await db('userSeq')
+        .where({ owner: uid, date: formattedDate })
+        .update({ seq });   
 
-    
     try {
-        await knex('tasks').insert({
+        await db('tasks').insert({
             owner: uid,       // TEXT, 반드시 문자열로 제공
             seq,         // INT
             content,     // TEXT
-            date,        // DATE (YYYY-MM-DD 형식의 문자열이나 Date 객체)
+            date: formattedDate,        // DATE (YYYY-MM-DD 형식의 문자열이나 Date 객체)
             priority,    // INT
             isCompleted: 0  // TINYINT (0 또는 1)
         });
-        res.status(200).json({ message: 'Task inserted' });
+        // Insert된 task의 seq 반환
+        res.status(200).json({ message: 'Task inserted', seq });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Insertion failed' });
@@ -52,18 +68,22 @@ router.get('/getTasks', async (req, res) => {
     }
 
     const date = req.query.date;
+    
     if (!date) {
         res.status(400).json({ message: 'Date is required' });
         return;
     }
 
+    const formattedDate = new Date(date).toISOString().slice(0, 10); // "YYYY-MM-DD"
+
+
     // 'tasks' 테이블에서 현재 요청과 owner와 date가 같은 행을 조회
-    // 단. priority가 낮은 순서로 정렬, priority가 같으면 seq가 낮은 순서로 정렬
-    const tasks = await knex('tasks')
-        .where({ owner: uid, date })
-        .orderBy(['priority', 'seq'])
-        .select('*');
-    
+    // 단. isCompleted가 0인 행을 먼저 조회, 1인 행은 가장 나중에 뜨게, isCompleted가 같으면 priority가 낮은 순으로 정렬, priority가 같으면 seq가 낮은 순으로 정렬
+    const tasks = await db('tasks')
+        .where({ owner: uid, date: formattedDate })
+        .orderByRaw('isCompleted ASC, priority ASC, seq ASC')
+        .select('seq', 'content', 'date', 'priority', 'isCompleted');
+
     if (tasks.length === 0) {
         res.status(404).json({ message: 'No tasks found' });
     } else {
@@ -90,14 +110,19 @@ router.post('/updateTask', async (req, res) => {
         res.status(400).json({ message: 'At least one field is required' });
         return;
     }
+    const formattedDate = new Date(date).toISOString().slice(0, 10); // "YYYY-MM-DD"
 
     const updateData = {};
     if (newContent) updateData.content = newContent;
-    if (newDate) updateData.date = newDate;
+    if (newDate) {
+        const formattedNewDate = new Date(newDate).toISOString().slice(0, 10); // "YYYY-MM-DD"
+        updateData.date = formattedNewDate;
+    } 
+
     if (newPriority) updateData.priority = newPriority;
     try {
-        await knex('tasks')
-            .where({ owner: uid, date, seq })
+        await db('tasks')
+            .where({ owner: uid, date: formattedDate, seq })
             .update(updateData);
         res.status(200).json({ message: 'Task updated' });
     } catch (error) {
@@ -106,7 +131,7 @@ router.post('/updateTask', async (req, res) => {
     }
 });
 
-router.post('/deleteTask', async (req, res) => {
+router.post('/removeTask', async (req, res) => {
     const {date, seq} = req.body;
     const uid =
         req.session &&
@@ -121,11 +146,14 @@ router.post('/deleteTask', async (req, res) => {
         res.status(400).json({ message: 'Date and seq are required' });
         return;
     }
+    const formattedDate = new Date(date).toISOString().slice(0, 10); // "YYYY-MM-DD"
+
     try {
-        await knex('tasks')
-            .where({ owner: uid, date, seq })
+        await db('tasks')
+            .where({ owner: uid, date: formattedDate, seq })
             .del();
         res.status(200).json({ message: 'Task deleted' });
+        console.log(uid, formattedDate, seq);
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Deletion failed' });
@@ -147,14 +175,48 @@ router.post('/completeTask', async (req, res) => {
         res.status(400).json({ message: 'Date and seq are required' });
         return;
     }
+    const formattedDate = new Date(date).toISOString().slice(0, 10); // "YYYY-MM-DD"
+
     try {
-        await knex('tasks')
-            .where({ owner: uid, date, seq })
+        await db('tasks')
+            .where({ owner: uid, date: formattedDate, seq })
             .update({ isCompleted: 1 });
         res.status(200).json({ message: 'Task completed' });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Completion failed' });
+    }
+});
+
+router.get('/isCompleted', async (req, res) => {
+    const { date, seq } = req.query;
+    const uid =
+        req.session &&
+        req.session.passport &&
+        req.session.passport.user &&
+        req.session.passport.user.id;
+    if (!uid) {
+        res.status(401).json({ message: 'Not authenticated' });
+        return;
+    }
+    if (!date || !seq) {
+        res.status(400).json({ message: 'Date and seq are required' });
+        return;
+    }
+    const formattedDate = new Date(date).toISOString().slice(0, 10); // "YYYY-MM-DD"
+    try {
+        const task = await db('tasks')
+            .where({ owner: uid, date: formattedDate, seq })
+            .select('isCompleted')
+            .first();
+        if (task) {
+            res.status(200).json({ isCompleted: task.isCompleted });
+        } else {
+            res.status(404).json({ message: 'Task not found' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error fetching task' });
     }
 });
 module.exports = router;
