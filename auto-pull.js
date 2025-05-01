996a0f3c1e132d5085a425307a43ab199af8f91f
shimaenaga1123 <shimaenaga1123@users.noreply.github.com>
2025-05-01 07:14:08 +0000

    Prettified Code!

--- auto-pull.js
+++ auto-pull.js
@@ -1,2 +1,4 @@
-const child_process = require('node:child_process');
-setInterval(() => { child_process.exec('git pull'); }, 60000);
+const child_process = require("node:child_process");
+setInterval(() => {
+  child_process.exec("git pull");
+}, 60000);
